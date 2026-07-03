996eccbc2cd92c94e703f6441490c0f72391a8e9
primeiroanoperola2024-collab <primeiroanoperola2024@gmail.com>
2026-07-03 08:59:00 -0300

    Add files via upload

---
+++ Atividade.02.js
@@ -0,0 +1,7 @@
+function mostrarMensagem(){
+    alert("Bem-vindo à Vieira Tech! Obrigado por visitar nosso site.");
+}
+
+function enviarMensagem(){
+    alert("Mensagem enviada com sucesso! Em breve entraremos em contato.");
+}
